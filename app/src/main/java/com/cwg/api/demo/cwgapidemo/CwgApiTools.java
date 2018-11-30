0808bfd07667293c7dd7c306786fd5676def8608
DEN <4040550@mail.ru>
2018-11-30 20:29:53 +0300

    Added debug log

--- app/src/main/java/com/cwg/api/demo/cwgapidemo/CwgApiTools.java
+++ app/src/main/java/com/cwg/api/demo/cwgapidemo/CwgApiTools.java
@@ -2,11 +2,14 @@ package com.cwg.api.demo.cwgapidemo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class CwgApiTools {
 
+    private static String TAG = "cwg-dbg";
 
     public static void sendApiInGeneralCommand(Context ctx, int cmd) {
+        Log.d(TAG, "sendApiInGeneralCommand cmd: " + cmd);
         Intent intent = new Intent();
         intent.setAction(CarwebguruAPI.Actions.API_IN_GENERAL);
         intent.putExtra(CarwebguruAPI.Data.CMD, cmd);
@@ -15,6 +18,7 @@ public class CwgApiTools {
 
 
     public static void sendApiInGeneralCommand(Context ctx, int cmd, String text) {
+        Log.d(TAG, "sendApiInGeneralCommand cmd: " + cmd + ", text: " + text);
         Intent intent = new Intent();
         intent.setAction(CarwebguruAPI.Actions.API_IN_GENERAL);
         intent.putExtra(CarwebguruAPI.Data.CMD, cmd);
@@ -24,16 +28,16 @@ public class CwgApiTools {
 
 
     public static void sendApiInTTSSay(Context ctx, String text) {
-        if (ctx != null) {
+        Log.d(TAG, "sendApiInTTSSay text: " + text);
             Intent intent = new Intent();
             intent.setAction(CarwebguruAPI.Actions.API_IN_TTS);
             intent.putExtra(CarwebguruAPI.Data.STRING, text);
             ctx.sendBroadcast(intent);
-        }
     }
 
 
     public static void sendApiInMediaCommand(Context ctx, int cmd) {
+        Log.d(TAG, "sendApiInMediaCommand cmd: " + cmd);
         Intent intent = new Intent();
         intent.setAction(CarwebguruAPI.Actions.API_IN_MEDIA);
         intent.putExtra(CarwebguruAPI.Data.CMD, cmd);
@@ -41,6 +45,7 @@ public class CwgApiTools {
     }
 
     public static void sendApiInLocationCommand(Context ctx, int cmd) {
+        Log.d(TAG, "sendApiInLocationCommand cmd: " + cmd);
         Intent intent = new Intent();
         intent.setAction(CarwebguruAPI.Actions.API_IN_LOCATION);
         intent.putExtra(CarwebguruAPI.Data.CMD, cmd);
@@ -49,6 +54,7 @@ public class CwgApiTools {
 
 
     public static void sendApiInGeneralWidget(Context ctx, int wIndex, String txtInfo) {
+        Log.d(TAG, "sendApiInGeneralWidget wIndex: " + wIndex + ", txtInfo: " + txtInfo);
         Intent intent = new Intent();
         intent.setAction(CarwebguruAPI.Actions.API_IN_GENERAL); // group for CWG API
         intent.putExtra("cmd", 10); // command: update widget
